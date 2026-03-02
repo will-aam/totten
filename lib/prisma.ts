c5e74237f7b2982e53cb8a8c51812e935b2cc611
Will <will00987654@gmail.com>
2026-03-02 17:13:59 -0300

    feat: enhance Prisma schema with new models and relationships, and add Prisma client initialization

---
+++ lib/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+// O construtor vazio buscará automaticamente a DATABASE_URL no seu .env
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
